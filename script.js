153851092a02c198ab412ea85964d9d0868c0ed9
shivamtg <shivamtg1376@gmail.com>
2022-07-25 12:29:48 +0200

    create image element for each image in js file

---
+++ script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_HEIGHT = canvas.height = 600;
+const CANVAS_WIDTH = canvas.width = 900;
+const GAME_SPEED = 5;
+
+const image1 = new Image();
+image1.src = 'images/01_ground.png';
+const image2 = new Image();
+image2.src = 'images/02_trees and bushes.png';
+const image3 = new Image();
+image3.src = 'images/03_distant_trees.png';
+const image4 = new Image();
+image4.src = 'images/04_bushes.png';
+const image5 = new Image();
+image5.src = 'images/05_hill1.png';
+const image6 = new Image();
+image6.src = 'images/06_hill2.png';
+const image7 = new Image();
+image7.src = 'images/07_huge_clouds.png';
+const image8 = new Image();
+image8.src = 'images/08_clouds.png';
+const image9 = new Image();
+image9.src = '09_distant_clouds1.png';
+const image10 = new Image();
+image10.src = 'images/10_distant_clouds.png';
+const image11 = new Image();
+image11.src = 'images/11_background.png';
+
+
